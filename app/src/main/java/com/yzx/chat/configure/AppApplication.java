551173f4068bd0e856c2d812c055740534067871
yezhixing <244546875@qq.com>
2017-11-14 17:03:36 +0800

    生命太短暂,不要去做一些根本没有人想要的东西

--- app/src/main/java/com/yzx/chat/configure/AppApplication.java
+++ app/src/main/java/com/yzx/chat/configure/AppApplication.java
@@ -24,37 +24,37 @@ public class AppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        String processAppName = getProcessName(this,android.os.Process.myPid());
-        if(processAppName!=null&&processAppName.equalsIgnoreCase(getPackageName())){
-            IdentityManager.init(this,
-                    Constants.PREFERENCES_AUTHENTICATION,
-                    Constants.RSA_KEY_ALIAS,
-                    Constants.AES_KEY_ALIAS,
-                    Constants.TOKEN_ALIAS,
-                    Constants.DEVICE_ID_ALIAS);
-
-            NetworkStateReceive.init(this);
-
-            AndroidTool.init(this);
-
+        String processAppName = getProcessName(this, android.os.Process.myPid());
+        if (processAppName != null && processAppName.equalsIgnoreCase(getPackageName())) {
+            initTool();
             initChat();
         }
     }
 
-    private void initChat() {
 
+    private void initTool() {
+        IdentityManager.init(this,
+                Constants.PREFERENCES_AUTHENTICATION,
+                Constants.RSA_KEY_ALIAS,
+                Constants.AES_KEY_ALIAS,
+                Constants.TOKEN_ALIAS,
+                Constants.DEVICE_ID_ALIAS);
+
+        NetworkStateReceive.init(this);
+
+        AndroidTool.init(this);
+    }
+
+    private void initChat() {
         EMOptions options = new EMOptions();
-        // 默认添加好友时，是不需要验证的，改成需要验证
         options.setAcceptInvitationAlways(false);
-        //初始化
         EMClient.getInstance().init(this, options);
-        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(false);
     }
 
     public static String getProcessName(Context cxt, int pid) {
         ActivityManager am = (ActivityManager) cxt.getSystemService(Context.ACTIVITY_SERVICE);
-        if(am==null){
+        if (am == null) {
             return null;
         }
         List<ActivityManager.RunningAppProcessInfo> runningApps = am.getRunningAppProcesses();
