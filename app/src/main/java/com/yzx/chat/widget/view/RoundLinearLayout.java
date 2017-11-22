c780df6738b2b57d39c8a7e19e948376edc7ec48
yezhixing <244546875@qq.com>
2017-11-22 18:24:07 +0800

    生命太短暂,不要去做一些根本没有人想要的东西

---
+++ app/src/main/java/com/yzx/chat/widget/view/RoundLinearLayout.java
@@ -0,0 +1,63 @@
+package com.yzx.chat.widget.view;
+
+import android.content.Context;
+import android.graphics.Outline;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.LinearLayout;
+
+/**
+ * Created by YZX on 2017年11月22日.
+ * 每一个不曾起舞的日子,都是对生命的辜负.
+ */
+
+public class RoundLinearLayout extends LinearLayout {
+
+    private float mRoundRadius;
+    private Context mContext;
+
+    public RoundLinearLayout(Context context) {
+        this(context, null);
+    }
+
+    public RoundLinearLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        setRound();
+    }
+
+    private void setRound() {
+        mRoundRadius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                4, mContext.getResources().getDisplayMetrics());
+        this.setClipToOutline(true);
+        this.setOutlineProvider(new RoundOutlineProvider(mRoundRadius));
+    }
+
+    private static class RoundOutlineProvider extends ViewOutlineProvider {
+        private float mRoundRadius;
+
+        RoundOutlineProvider(float roundRadius) {
+            mRoundRadius = roundRadius;
+        }
+
+        @Override
+        public void getOutline(View view, Outline outline) {
+            outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), mRoundRadius);
+        }
+    }
+
+    public float getRoundRadius() {
+        return mRoundRadius;
+    }
+
+    public void setRoundRadius(float roundRadius) {
+        mRoundRadius = roundRadius;
+        this.setOutlineProvider(new RoundOutlineProvider(mRoundRadius));
+    }
+}
