a69f2b1196d08c0601911e73715bb369687bcace
yezhixing <244546875@qq.com>
2018-02-06 23:48:10 +0800

    每一个不曾起舞的日子 都是对生命的辜负。

--- app/src/main/java/com/yzx/chat/view/activity/ProfileModifyActivity.java
+++ app/src/main/java/com/yzx/chat/view/activity/ProfileModifyActivity.java
@@ -2,11 +2,13 @@ package com.yzx.chat.view.activity;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.text.method.CharacterPickerDialog;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.yzx.chat.R;
 import com.yzx.chat.base.BaseCompatActivity;
 import com.yzx.chat.bean.UserBean;
@@ -23,6 +25,10 @@ public class ProfileModifyActivity extends BaseCompatActivity {
 
     private LinearLayout mLlBirthday;
     private TextView mTvBirthday;
+    private LinearLayout mLlSexSelected;
+    private TextView mTvSexSelected;
+    private LinearLayout mLlLocation;
+    private TextView mTvLocation;
     private UserBean mUserBean;
 
     @Override
@@ -34,6 +40,10 @@ public class ProfileModifyActivity extends BaseCompatActivity {
     protected void init() {
         mLlBirthday = findViewById(R.id.ProfileModifyActivity_mLlBirthday);
         mTvBirthday = findViewById(R.id.ProfileModifyActivity_mTvBirthday);
+        mLlSexSelected = findViewById(R.id.ProfileModifyActivity_mLlSexSelected);
+        mTvSexSelected = findViewById(R.id.ProfileModifyActivity_mTvSexSelected);
+        mLlLocation = findViewById(R.id.ProfileModifyActivity_mLlLocation);
+        mTvLocation = findViewById(R.id.ProfileModifyActivity_mTvLocation);
     }
 
     @Override
@@ -41,7 +51,10 @@ public class ProfileModifyActivity extends BaseCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        mLlSexSelected.setOnClickListener(mOnSexSelectedClickListener);
         mLlBirthday.setOnClickListener(mOnBirthdayClickListener);
+        mLlLocation.setOnClickListener(mOnLocationClickListener);
 
         setData();
     }
@@ -50,6 +63,30 @@ public class ProfileModifyActivity extends BaseCompatActivity {
         mUserBean = new UserBean();
 
     }
+
+    private final View.OnClickListener mOnLocationClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private final View.OnClickListener mOnSexSelectedClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            new MaterialDialog.Builder(ProfileModifyActivity.this)
+                    .title(R.string.ProfileModifyActivity_SexDialogTitle)
+                    .items(R.array.ProfileModifyActivity_SexList)
+                    .itemsCallback(new MaterialDialog.ListCallback() {
+                        @Override
+                        public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
+                            mTvSexSelected.setText(text);
+                        }
+                    })
+                    .show();
+        }
+    };
+
 
     private final View.OnClickListener mOnBirthdayClickListener = new View.OnClickListener() {
         private DatePickerDialog mDatePickerDialog;
@@ -60,13 +97,13 @@ public class ProfileModifyActivity extends BaseCompatActivity {
                 mDatePickerDialog = new DatePickerDialog(ProfileModifyActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        LogUtil.e(year + " " + month + " " + dayOfMonth);
+                        mTvBirthday.setText(String.format(getString(R.string.ProfileModifyActivity_DateFormat), year, month, dayOfMonth));
                     }
                 }, -1, -1, -1);
-                mDatePickerDialog.setTitle(getString(R.string.ProfileModifyActivity_BirthdayDialogTitle));
             }
             Calendar calendar = Calendar.getInstance();
             mDatePickerDialog.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+            mDatePickerDialog.setTitle(getString(R.string.ProfileModifyActivity_BirthdayDialogTitle));
             mDatePickerDialog.show();
         }
     };
