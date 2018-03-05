62d76af5f4729915b5996328ab47c84074d33821
yezhixing <244546875@qq.com>
2018-03-06 01:19:57 +0800

    每一个不曾起舞的日子,都是对生命的辜负

--- app/src/main/java/com/yzx/chat/network/chat/IMClient.java
+++ app/src/main/java/com/yzx/chat/network/chat/IMClient.java
@@ -2,6 +2,26 @@ package com.yzx.chat.network.chat;
 
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.yzx.chat.base.BaseHttpCallback;
+import com.yzx.chat.bean.ContactBean;
+import com.yzx.chat.bean.UserBean;
+import com.yzx.chat.configure.Constants;
+import com.yzx.chat.database.AbstractDao;
+import com.yzx.chat.database.DBHelper;
+import com.yzx.chat.network.api.JsonResponse;
+import com.yzx.chat.network.api.auth.AuthApi;
+import com.yzx.chat.network.api.auth.TokenVerifyBean;
+import com.yzx.chat.network.api.contact.ContactApi;
+import com.yzx.chat.network.api.contact.GetUserContactsBean;
+import com.yzx.chat.network.framework.HttpCallback;
+import com.yzx.chat.network.framework.HttpResponse;
+import com.yzx.chat.tool.ApiHelper;
+import com.yzx.chat.tool.IdentityManager;
+import com.yzx.chat.util.AsyncUtil;
+import com.yzx.chat.util.LogUtil;
+import com.yzx.chat.util.MD5Util;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -41,8 +61,9 @@ public class IMClient {
     private ChatManager mChatManager;
     private ContactManager mContactManager;
     private ConversationManager mConversationManager;
-    private List<OnConnectionStateChangeListener> mOnConnectionStateChangeListenerList;
     private ThreadPoolExecutor mWorkExecutor;
+    private DBHelper mDBHelper;
+    private List<OnConnectionStateChangeListener> mOnConnectionStateChangeListenerList;
 
     private boolean isLogged;
 
@@ -93,12 +114,86 @@ public class IMClient {
 
             private void loginSuccess(boolean isConnectedToServer) {
                 isLogged = true;
+                mDBHelper = new DBHelper(sAppContext, MD5Util.encrypt(IdentityManager.getInstance().getUserID()), Constants.DATABASE_VERSION);
                 mChatManager = new ChatManager(mSubManagerCallback);
                 mContactManager = new ContactManager(mSubManagerCallback);
                 mConversationManager = new ConversationManager(mSubManagerCallback);
                 loginListener.onLoginSuccess(isConnectedToServer);
             }
         });
+    }
+
+    private void initHTTPServer(boolean isAlreadyLogged) {
+        ContactApi contactApi = (ContactApi) ApiHelper.getProxyInstance(ContactApi.class);
+        AuthApi authApi = (AuthApi) ApiHelper.getProxyInstance(AuthApi.class);
+
+        AsyncUtil.cancelCall(mTokenVerify);
+        mTokenVerify = authApi.tokenVerify();
+        mTokenVerify.setCallback(new HttpCallback<JsonResponse<TokenVerifyBean>>() {
+            private boolean isSuccess;
+
+            @Override
+            public void onResponse(HttpResponse<JsonResponse<TokenVerifyBean>> response) {
+                if (response.getResponseCode() == 200) {
+                    JsonResponse<TokenVerifyBean> jsonResponse = response.getResponse();
+                    if (jsonResponse != null) {
+                        TokenVerifyBean tokenVerifyBean = jsonResponse.getData();
+                        if (jsonResponse.getStatus() == 200 && tokenVerifyBean != null) {
+                            UserBean userBean = tokenVerifyBean.getUser();
+                            if (userBean != null && !userBean.isEmpty() && IdentityManager.getInstance().updateUserInfo(userBean)) {
+                                isSuccess = true;
+                                return;
+                            }
+                        }
+                    }
+                }
+                initComplete();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                e.printStackTrace();
+                if (IdentityManager.initFromLocal()) {
+                    isSuccess = true;
+                } else {
+                    mSplashView.startLoginActivity();
+                }
+            }
+
+            @Override
+            public boolean isExecuteNextTask() {
+                return isSuccess;
+            }
+        });
+
+        AsyncUtil.cancelCall(mGetUserFriendsTask);
+        mGetUserFriendsTask = contactApi.getUserContacts();
+        mGetUserFriendsTask.setCallback(new BaseHttpCallback<GetUserContactsBean>() {
+            @Override
+            protected void onSuccess(GetUserContactsBean response) {
+                List<ContactBean> contactBeans = response.getContacts();
+                if (contactBeans != null) {
+                    IMClient.getInstance().contactManager().initContacts(contactBeans);
+                } else {
+                    LogUtil.e("response.getContacts() is null");
+                    IMClient.getInstance().contactManager().initContactsFromDB();
+                }
+                isInitHTTPComplete = true;
+                initComplete();
+            }
+
+            @Override
+            protected void onFailure(String message) {
+                LogUtil.e(message);
+                IMClient.getInstance().contactManager().initContactsFromDB();
+                initComplete();
+            }
+        });
+        if (isAlreadyLogged) {
+            sHttpExecutor.submit(mGetUserFriendsTask);
+        } else {
+            sHttpExecutor.submit(mTokenVerify, mGetUserFriendsTask);
+        }
     }
 
     public void logout() {
@@ -108,6 +203,11 @@ public class IMClient {
             mChatManager.destroy();
             mContactManager.destroy();
             mConversationManager.destroy();
+            mDBHelper.destroy();
+            mChatManager = null;
+            mContactManager = null;
+            mConversationManager = null;
+            mDBHelper = null;
         }
     }
 
@@ -142,6 +242,20 @@ public class IMClient {
         mOnConnectionStateChangeListenerList.remove(listener);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     private final SubManagerCallback mSubManagerCallback = new SubManagerCallback() {
         @Override
         public void chatManagerCallback(int callbackCode, Object arg) {
@@ -160,6 +274,11 @@ public class IMClient {
         @Override
         public void contactManagerCallback(int callbackCode, Object arg) {
 
+        }
+
+        @Override
+        public AbstractDao.ReadWriteHelper getDatabaseReadWriteHelper() {
+            return mDBHelper.getReadWriteHelper();
         }
 
         @Override
@@ -234,6 +353,8 @@ public class IMClient {
         void conversationManagerCallback(int callbackCode, Object arg);
 
         void contactManagerCallback(int callbackCode, Object arg);
+
+        AbstractDao.ReadWriteHelper getDatabaseReadWriteHelper();
 
         void execute(Runnable runnable);
     }
